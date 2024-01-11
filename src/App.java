c5f5d6bea0fc0644268d4672bc4ea4860a032567
Adrian R <roblesadrian03@gmail.com>
2024-01-11 17:41:16 +0100

    Actualización comentarios App.java

--- src/App.java
+++ src/App.java
@@ -3,17 +3,25 @@ import java.util.NoSuchElementException;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // -- Inicialización de objetos necesarios (Gestor,bibliotecas, libros pre-cargados)
+
+        // Gestor y array de bibliotecas
+
         GestorBiblioteca gBiblioteca = new GestorBiblioteca();
         Biblioteca[] bibliotecas = new Biblioteca[2];
 
         Libro[] librosB1 = new Libro[10];
         Libro[] librosB2 = new Libro[10];
 
+        // Bibliotecas
+
         Biblioteca b1 = new Biblioteca("Isaac Albeniz", "Avenida de las Lagunas s/n", librosB1);
         Biblioteca b2 = new Biblioteca("Gloria Fuertes", "Calle Isabel II s/n", librosB2);
 
         bibliotecas[0] = b1;
         bibliotecas[1] = b2;
+
+        // Libros pre-cargados
 
         Libro libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967, 9780307474728L, 2);
         Libro libro2 = new Libro("El Principito", "Antoine de Saint-Exupéry", 1943, 9780156013925L, 3);
@@ -22,15 +30,25 @@ public class App {
         Libro libro5 = new Libro("El código Da Vinci", "Dan Brown", 2003, 9788495618600L, 5);
         Libro libro6 = new Libro("Orgullo y prejuicio", "Jane Austen", 1813, 9788491052425L, 1);
 
+        // -- Asignación de libros a cada biblioteca
+
+        // Biblioteca 1
+
         librosB1[0] = libro1;
         librosB1[1] = libro2;
         librosB1[2] = libro3;
+
+        // Biblioteca 2
 
         librosB2[0] = libro4;
         librosB2[1] = libro5;
         librosB2[2] = libro6;
 
         int optionSelector = 0;
+
+        // Ejecución menú
+
+        // PENDIENTE. -- Añadir un selector previo al menú que permita diferenciar la ejecución de los métodos sobre una biblioteca u otra
 
         do {
             System.out.println(
@@ -48,7 +66,7 @@ public class App {
                     break;
                 case 2: gBiblioteca.devolverLibro();
                     break;
-                case 3: b1.anadirLibros(); // Las opciones asignadas aquí no corresponden con las del menú 
+                case 3: b1.anadirLibros(); 
                     break;
                 case 4: gBiblioteca.eliminarLibro();
                     break;
